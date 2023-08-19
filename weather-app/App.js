074d77e0e1ca2fa412f99dc15338a1a108a82592
ialim0 <i.alim0229@gmail.com>
2023-08-19 19:16:41 +0000

    Display Input

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,14 +1,19 @@
-import { Text, View,ImageBackground } from "react-native";
+import React, { useState } from 'react'
+import { Text, View,ImageBackground  } from "react-native";
 import {s} from "./App.style"
 import hotImage from "./assets/hot.png"
 import { InputTemperature } from "./components/InputTemperature.style.jsx";
 import {TemperatureDisplay} from "./components/TemperatureDisplay.style.jsx";
+import { DEFAULT_UNITS,DEFAULT_TEMPERATURE } from "./constant";
 export default function App() {
+  const [inputValue, setInputValue]= useState("0"); 
+  const [currentUnit,setCurrentUnit]= useState("°C");
   return (
+   
     <ImageBackground source={hotImage}style={s.container} >
       <View style={s.workspace}>
-       <TemperatureDisplay value="1234" unit="°C"/> 
-        <InputTemperature  defaultValue={"12 "}   />
+       <TemperatureDisplay value={inputValue} unit={DEFAULT_UNITS}/> 
+        <InputTemperature onChangeText={setInputValue}   defaultValue={DEFAULT_TEMPERATURE}   />
       
         <View>
         <Text>Temperature  </Text>
---
+++ weather-app/constant.js
@@ -0,0 +1,6 @@
+export const DEFAULT_TEMPERATURE="0"
+export const UNITS ={
+    celcius:"°C",
+    faranheit:"°F",
+}
+export const DEFAULT_UNITS=UNITS.celcius
